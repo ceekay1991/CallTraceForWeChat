9185253fde4c20e3cf73566f4d7864b202ef097c
chenronghang <chenronghang@baidu.com>
2020-03-23 11:08:34 +0800

    添加bbasm_calltrace_fake_objc_msgSend_safe实现的一些注释，便于阅读代码

--- CallTraceForWeChat/CallTraceForWeChatDylib/Monitor/TimeProfiler/BBASMCallTrace.c
+++ CallTraceForWeChat/CallTraceForWeChatDylib/Monitor/TimeProfiler/BBASMCallTrace.c
@@ -62,6 +62,7 @@ void bbasm_calltrace_setIgnoreClass(Class *cls,uint64_t count) {
 }
 
 
+
 void bbasm_calltrace_setMonitorThreadName(const char * name) {
     bbasm_calltrace_monitorThreadName = name;
 }
@@ -205,30 +206,6 @@ static inline uintptr_t bbasm_calltrace_getLRRegisterValue() {
     return lr;
 }
 
-//void bbasm_calltrace_hook_objc_msgSend_before(id self, SEL sel, uintptr_t lr) {
-//    bool isMain = pthread_main_np() != 0;
-//    if (bbasm_calltrace_callRecordEnable && bbasm_calltrace_matchMonitorThread()) {
-//        Class cls = object_getClass(self);
-//        if (cls) {
-//            bool ignore = false;
-//            for (int i = 0; i < bbasm_calltrace_ignore_class_count; ++i) {
-//                Class ignoreclass = bbasm_calltrace_class_ignore[i];
-//                if (ignoreclass == cls) {
-//                    ignore = true;
-//                    break;
-//                }
-//            }
-//            if (!ignore) {
-//                bbasm_calltrace_pushCallRecord(cls, sel, isMain);
-//            }
-//        } else {
-//            // 合理情况 当调用基础库的safe api 比如字符串判空等触发
-//            printf("bbasm_call_trace_error:class is null\n");
-//        }
-//
-//    }
-//    bbasm_calltrace_setLRRegisterValue(lr);
-//}
 
 void bbasm_calltrace_hook_objc_msgSend_before(id self, SEL sel, uintptr_t lr) {
     if (bbasm_calltrace_callRecordEnable && bbasm_calltrace_matchMonitorThread()) {
@@ -248,13 +225,57 @@ uintptr_t bbasm_calltrace_hook_objc_msgSend_after() {
 }
 
 
+/*
+ * arm64程序调用规则
+ * https://juejin.im/post/5d14623ef265da1bb47d7635
+ * arm64 objc_msgSend 源码
+ * https://opensource.apple.com/source/objc4/objc4-723/runtime/Messengers.subproj/objc-msg-arm64.s.auto.html
+ * ARM64汇编学习笔记二(寄存器和函数本质)
+ * https://www.jianshu.com/p/cf29fb303bdc
+ * objc_msgSend 方法执行的逻辑是：先获取对象对应类的信息，再获取方法的缓存，根据方法的 selector 查找函数指针，经过异常错误处理后，最后跳到对应函数的实现。
+ * ARM64经常用到的汇编指令
+ *##################################################################################
+ * MOV    X1，X0         ;将寄存器X0的值传送到寄存器X1
+ * ADD    X0，X1，X2     ;寄存器X1和X2的值相加后传送到X0
+ * SUB    X0，X1，X2     ;寄存器X1和X2的值相减后传送到X0
 
+ * AND    X0，X0，#0xF    ; X0的值与0xF相位与后的值传送到X0
+ * ORR    X0，X0，#9      ; X0的值与9相位或后的值传送到X0
+ * EOR    X0，X0，#0xF    ; X0的值与0xF相异或后的值传送到X0
+
+ * LDR    X5，[X6，#0x08]        ；ld：load; X6寄存器加0x08的和的地址值内的数据传送到X5
+ * LDP  x29, x30, [sp, #0x10]    ; ldp :load pair ; 一对寄存器, 从内存读取数据到寄存器
+
+ * STR X0, [SP, #0x8]         ；st:store,str:往内存中写数据（偏移值为正）; X0寄存器的数据传送到SP+0x8地址值指向的存储空间
+ * STUR   w0, [x29, #-0x8]   ;往内存中写数据（偏移值为负）
+ * STP  x29, x30, [sp, #0x10]    ;store pair，存放一对数据, 入栈指令
+
+ * CBZ  ;比较（Compare），如果结果为零（Zero）就转移（只能跳到后面的指令）
+ * CBNZ ;比较，如果结果非零（Non Zero）就转移（只能跳到后面的指令）
+ * CMP  ;比较指令，相当于SUBS，影响程序状态寄存器CPSR
+
+ * B   ;跳转指令，可带条件跳转与cmp配合使用
+ * BL  ;带返回的跳转指令， 返回地址保存到LR（X30）
+ * BLR  ; 带返回的跳转指令，跳转到指令后边跟随寄存器中保存的地址(例：blr    x8 ;跳转到x8保存的地址中去执行)
+ * RET   ;子程序返回指令，返回地址默认保存在LR（X30）
+ *##################################################################################
+ */
 #define bbasm_calltrace_call(b, value) \
 __asm volatile ("stp x8, x9, [sp, #-16]!\n"); \
 __asm volatile ("mov x10, %0\n" :: "r"(value)); \
 __asm volatile ("ldp x8, x9, [sp], #16\n"); \
 __asm volatile (#b " x10\n");
 
+/*
+ * bbasm_calltrace_fake_objc_msgSend_safe
+ *  保存寄存器x0 - x8，q0 - q7到栈上，
+ *  记录class，selector，startTime，lr
+ *  恢复寄存器x0 - x8，q0 - q7，
+ *  调用原始objc_msg_send，
+ *  保存寄存器x0 - x8，q0 - q7到栈上，
+ *  记录endTime，
+ *  恢复寄存器x0 - x8，q0 - q7，以及lr
+ */
 __attribute__((__naked__))
 static void bbasm_calltrace_fake_objc_msgSend_safe() {
     //维护CFI(call frame information)，这样就可以看到调用堆栈
@@ -267,11 +288,14 @@ static void bbasm_calltrace_fake_objc_msgSend_safe() {
 //    );
     // backup registers
     __asm__ volatile(
+                     // sp 是堆栈寄存器，存放栈的偏移地址，每次都指向栈顶。
+                     // 保存 {q0-q7} 偏移地址到 sp 寄存器
                      "stp q6, q7, [sp, #-0x20]!\n"
                      "stp q4, q5, [sp, #-0x20]!\n"
                      "stp q2, q3, [sp, #-0x20]!\n"
                      "stp q0, q1, [sp, #-0x20]!\n"
                      "str x8,  [sp, #-16]!\n"  //arm64标准：sp % 16 必须等于0
+                     // 保存 {x0-x8}
                      "stp x6, x7, [sp, #-16]!\n"
                      "stp x4, x5, [sp, #-16]!\n"
                      "stp x2, x3, [sp, #-16]!\n"
@@ -283,23 +307,26 @@ static void bbasm_calltrace_fake_objc_msgSend_safe() {
                      hook_objc_msgSend_before(id self, SEL sel, uintptr_t lr)
                      x0=self  x1=sel x2=lr
                      */
+                    // 交换参数,调用bbasm_calltrace_hook_objc_msgSend_before
                     "mov x2, lr\n"
                     "bl _bbasm_calltrace_hook_objc_msgSend_before"
                     );
     
     // restore registers
     __asm volatile (
+                    // 读取 {x0-x8} 从保存到 sp 栈顶的偏移地址读起
                     "ldp x0, x1, [sp], #16\n"
                     "ldp x2, x3, [sp], #16\n"
                     "ldp x4, x5, [sp], #16\n"
                     "ldp x6, x7, [sp], #16\n"
                     "ldr x8,  [sp], #16\n"
+                    // 读取 {q0-q7}
                     "ldp q0, q1, [sp], #0x20\n"
                     "ldp q2, q3, [sp], #0x20\n"
                     "ldp q4, q5, [sp], #0x20\n"
                     "ldp q6, q7, [sp], #0x20\n"
                     );
-    
+    // 调用原始 objc_msgSend
     bbasm_calltrace_call(blr, bbasm_calltrace_orgin_objc_msgSend)
 
     // backup registers
